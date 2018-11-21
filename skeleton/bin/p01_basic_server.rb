0b0a95e88455df3eba094b2595fa9a0cb95b01eb
sameehkhan <sameehkhan1@gmail.com>
2018-11-21 14:58:05 -0800

    Create controller server

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -1,1 +1,29 @@
 require 'rack'
+
+# Rack::Server.start({
+#   app: app.call(env),
+#   Port: 3000
+# })
+# 
+# app = Proc.new do |env|
+#   req = Rack::Request.new(env)
+#   res = Rack::Response.new
+#   res['Content-Type'] = 'text/html'
+#   res.write("Hello world!")
+#   res.finish
+# end
+
+class MyRackApp
+  def self.call(env)
+    req = Rack::Request.new(env)
+    res = Rack::Response.new
+
+    res.write("Hello world!")
+    res.finish
+  end
+end
+
+Rack::Server.start({
+  app: MyRackApp,
+  Port: 3000
+})
--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -8,25 +8,53 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req 
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    !!@already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    if already_built_response?
+      raise 'error'
+    else
+      @res.status = 302 
+      @res["Location"] = url 
+      @already_built_response = true
+    end 
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
-  def render_content(content, content_type)
+  def render_content(content, content_type="text/html")
+    if already_built_response?
+      raise 'error'
+    else
+      @res.write(content)
+      @res['Content-Type'] = content_type
+      @already_built_response = true
+    end 
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    template_path = File.join(
+    dir_path, "views", "#{template_name}.html.erb"
+    )
+
+    template_code = File.read(template_path)
+
+    render_content(
+    ERB.new(template_code).result(binding),
+    "text/html"
+    ) 
   end
 
   # method exposing a `Session` object
