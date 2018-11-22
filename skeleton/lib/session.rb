453e5565a35524964bbdbbc51f5ab559de1e2753
sameehkhan <sameehkhan1@gmail.com>
2018-11-21 16:32:14 -0800

    Parse cookie

--- skeleton/lib/session.rb
+++ skeleton/lib/session.rb
@@ -4,12 +4,21 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    session = Hash.new
+    @cookie = JSON.parse(req.cookies['_rails_lite_app'])
+    
+    session[@cookie.keys[0]] = @cookie.values[0]
+
+  
+
   end
 
   def [](key)
+    @cookie[key]
   end
 
   def []=(key, val)
+    @cookie[key] = val 
   end
 
   # serialize the hash into json and save in a cookie
